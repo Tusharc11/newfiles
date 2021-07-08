3fa32e148b88640898c8bea44d87b68573a91d28
Tusharc11 <tusharashokc11@gmail.com>
2021-07-08 09:56:55 +0530

    first comit

---
+++ myapp/src/main/java/myapp/loginpage.java
@@ -0,0 +1,11 @@
+package myapp;
+
+public class loginpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is login page");
+
+	}
+
+}
